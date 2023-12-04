4d3c93f80f3664d7428900a6fee02a9b365dbdf5
Ícaro Levi Milet de Albuquerque Soares <CIANDT\icaromilet@lnb029327cps.ciandt.global>
2023-12-04 08:27:08 -0300

    arrays and objects

---
+++ src/javascript/fundamentals/object/object.js
@@ -0,0 +1,16 @@
+const product = {}
+product.name = 'iPhone'
+product.price = 10000
+product['category of product'] = 'cellphone' //avoid blank space assignments
+
+console.log(product)
+
+const product2 = { 
+    name: 't-shirt',
+    price: 45,
+    object: {
+        object: {
+            
+        }
+    }
+}
